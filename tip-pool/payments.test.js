525e7e2d26b5ebc2dd92f4a74d49a38af408cfdc
John Finlay <finlay_john@hotmail.com>
2022-05-16 06:42:07 -0500

    add payments.test.js

---
+++ tip-pool/payments.test.js
@@ -0,0 +1,17 @@
+describe('Payments test', function() {
+  beforeEach(function() {
+
+  });
+  //submitPaymentInfo()
+    it('should add a new payment to allPayments on submitPaymentInfo()', function() {
+
+    });
+  //createCurPayment()
+
+  //appendPaymentTable()
+
+  //updateSummary()
+  afterEach(function() {
+
+  });
+});
