7a78e6422663f59895691dbf4237930f371e9f96
John Finlay <finlay_john@hotmail.com>
2022-05-17 06:17:44 -0500

    added helpers.test.js

---
+++ tip-pool/helpers.test.js
@@ -0,0 +1,18 @@
+describe('Helpers test', function() {
+// beforeEach
+  beforeEach(function() {
+
+  });
+// sumPaymentTotal(type)
+  it('should return the total amount of bills from allPayments', function() {
+
+  });
+// calculateTipPercent(billAmt, tipAmt)
+
+// appendTd(tr, value)
+
+// afterEach
+  afterEach(function() {
+
+  });
+});
